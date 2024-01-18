e8bc2ea187d49fd49e24df1a1ac18a2d68b75bcd
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-01-18 12:58:51 -0800

    added card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Card from '/src/components/Card.jsx'
 import Header from '/src/components/Header.jsx'
+import data from './data'
 export default function App() {
+  const cardComponents = data.map(element =>{
+    return (<Card
+      key={element.id}
+      item={element}
+    />)
+})
   return (
-    <div className='container'>
+    <div>
       <Header />
-      <Card />
+      <section className="cards-list">
+                {cardComponents}
+      </section>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
-export default function Card(){
+import locationIcon from '/images/location-icon.svg'
+export default function Card(props){
     return(
-    <section>
-        <p>this is a card section</p>
+    <section className='card'>
+        <img className='card-img' src={props.item.imageUrl}/>
+        <div className='card-details'>
+            <div className='location-details'>
+                <img className='location-icon' src={locationIcon}/>
+                <h3>{props.item.location}</h3>
+                <a href={props.item.googleMapsUrl} target='_blank'>View on Google Maps</a>
+            </div>
+            <h1>{props.item.title}</h1>
+            <h3>{props.item.startDate} - {props.item.endDate}</h3>
+            <p>{props.item.description}</p>
+        </div>
     </section>
     )
 }
